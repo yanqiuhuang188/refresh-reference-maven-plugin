9a66ba8c9d0063ee4322b0450f661efbc8b07580
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-06-03 22:51:25 +0800

    update

--- src/main/java/com/nimble/refreshReference.java
+++ src/main/java/com/nimble/refreshReference.java
@@ -1,8 +1,14 @@
 package com.nimble;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 /**
@@ -15,8 +21,26 @@ public class refreshReference extends AbstractMojo {
     private String[] paths;
 
     public void execute() {
-        for(String path : paths){
-            System.out.println("path = " + path);
+        try {
+            for (String item : paths) {
+
+                Path path = Paths.get(item);
+                if(Files.exists(path)){
+                    // Directory
+                    if (Files.isDirectory(path)) {
+                        FileUtils.deleteDirectory(path.toFile());
+                    } else {
+                        Files.deleteIfExists(path);
+                    }
+                    System.out.println("Ok : " + path.toString() + " had delete !");
+                }else {
+                    System.out.println("Warn : " + path.toString() + " is not exist !");
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error, cause by " + e.getClass().getName());
+            e.printStackTrace();
         }
     }
 }
